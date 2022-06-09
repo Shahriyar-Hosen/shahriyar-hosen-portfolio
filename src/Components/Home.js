b98150aa944af898d46175b97549f9a3e1866049
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-06-09 12:53:03 +0600

    Home section full done

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,35 @@
 import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
 
 const Home = () => {
   return (
-    <div className="w-full h-screen bg-[#0a192f]">
-      <h1>This Home page</h1>
-    </div>
+    <section name="home" className="w-full h-screen bg-[#0a192f]">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-pink-600">Hi, my name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+          Shahriyar Hosen
+        </h1>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+          I'm a Mern Stack Developer.
+        </h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">
+          I am Shahriar Hossain. Marn-Stack is a hard-working web developer in
+          Bangladesh. I usually create applications with react.js. Before I
+          start any project with the importance of UI / UX in each project. My
+          goal is to finish the job on time and do my best to get the job done
+          Nicely.
+        </p>
+        <div>
+          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600 duration-200">
+            View Projects
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-3 " />
+            </span>
+          </button>
+        </div>
+      </div>
+    </section>
   );
 };
 
