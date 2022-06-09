1d8e64f4a55586e64e625345a80ea0d0b93a3b61
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-06-09 19:23:01 +0600

    react-scroll done

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -11,10 +11,12 @@ const Contact = () => {
         action="https://getform.io/f/edbacf8a-d2b7-4aef-96fe-83630c5b10df"
         className="flex flex-col max-w-[600px] w-full"
       >
-        <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">
-            Contact
-          </p>
+        <div className="flex justify-center pt-10">
+        <h1 className="text-5xl font-bold inline border-b-4 border-pink-600 mt-10 text-gray-300">
+        Contact
+        </h1>
+      </div>
+        <div className="py-8">
           <p className="text-gray-300 py-4">
             / / Submit the form below or shoot me an email - myemail@gmail.com
           </p>
@@ -24,18 +26,21 @@ const Contact = () => {
           type="text"
           placeholder="Name"
           name="name"
+          required
         />
         <input
           className="my-4 p-2 bg-[#ccd6f6]"
           type="email"
           placeholder="Email"
           name="email"
+          required
         />
         <textarea
           className="bg-[#ccd6f6] p-2"
           name="message"
           rows="10"
           placeholder="Message"
+          required
         ></textarea>
         <button className="text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center">
           Let's Collaborate
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -37,20 +37,18 @@ const Navbar = () => {
         </li>
         <li>
           <Link to="skills" smooth={true} duration={500}>
-          Skills
+            Skills
           </Link>
         </li>
         <li>
           <Link to="project" smooth={true} duration={500}>
-          Projects
+            Projects
           </Link>
-          
         </li>
         <li>
           <Link to="contact" smooth={true} duration={500}>
-          Contact
+            Contact
           </Link>
-          
         </li>
       </ul>
 
@@ -67,11 +65,33 @@ const Navbar = () => {
             : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
         }
       >
-        <li className="py-6 text-4xl">Home</li>
-        <li className="py-6 text-4xl">About</li>
-        <li className="py-6 text-4xl">Skills</li>
-        <li className="py-6 text-4xl">Projects</li>
-        <li className="py-6 text-4xl">Contact</li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          {" "}
+          <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="project" smooth={true} duration={500}>
+            Projects
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          {" "}
+          <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
       </ul>
 
       {/* Social Icon */}
